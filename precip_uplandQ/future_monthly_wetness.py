3456f9d01b33824c21388a900c2939fd5e8a38e0
Eric White <eric.davidson.white@gmail.com>
2026-04-27 14:52:01 -0500

    update code to remove dashes from date format
    now using YYYYMMDD instead of YYYY-MM-DD

--- precip_uplandQ/future_monthly_wetness.py
+++ precip_uplandQ/future_monthly_wetness.py
@@ -14,7 +14,7 @@ conditions_df = pd.read_csv(conditions_file)
 
 # daily observed data for all compartments
 observed_file = "AORC_Precip_UplandQ_Daily_2000_2023.csv"
-observed_df = pd.read_csv(observed_file, parse_dates=["yyyy-mm-dd"])
+observed_df = pd.read_csv(observed_file, parse_dates=["yyyymmdd"])
 
 # future conditions based on cmip6 data
 future_conditions = "../CMIP6_precipitation/CMIP6_ensemble_median_NWS_anomaly_wetdry_monthly.csv"
@@ -23,8 +23,8 @@ future_df = pd.read_csv(future_conditions)
 print(f'Building timeseries from monthly wetness classes for {start_year} through {end_year} under: {SSP}')
 
 # add columns for month/year lookups
-observed_df["year"] = observed_df["yyyy-mm-dd"].dt.year
-observed_df["month"] = observed_df["yyyy-mm-dd"].dt.month
+observed_df["year"] = observed_df["yyyymmdd"].dt.year
+observed_df["month"] = observed_df["yyyymmdd"].dt.month
 
 output_blocks = []
 
@@ -53,13 +53,13 @@ while start <= end:
         historical_monthly_data = observed_df.loc[
             (observed_df["year"] == historical_year) & (observed_df["month"] == month) ].copy()
 
-    days_in_month = historical_monthly_data["yyyy-mm-dd"].dt.day.values  # array of month days
+    days_in_month = historical_monthly_data["yyyymmdd"].dt.day.values  # array of month days
     # remove leap days from historic datetime list being used as representative data for the month
     if max(days_in_month) == 29:
         days_in_month = days_in_month[0:-1]
     
     # change the timestamps in the monthly data from the historical date to the future date
-    historical_monthly_data["yyyy-mm-dd"] = [datetime(year, month, day) for day in days_in_month]
+    historical_monthly_data["yyyymmdd"] = [datetime(year, month, day) for day in days_in_month]
 
     output_blocks.append(historical_monthly_data.drop(columns=["year", "month"]))
 
@@ -70,14 +70,14 @@ future_timeseries_df = pd.concat(output_blocks, ignore_index=True)
 #future_timeseries_df.to_csv(output_filename_no_leap, index=True, index_label="yyyy-mm-dd")
 
 # address leap years at the end here - add leap days back into daily timeseries and first fill with NA, then interpolate across NA data
-future_timeseries_df['yyyy-mm-dd'] = pd.to_datetime(future_timeseries_df['yyyy-mm-dd'])
-future_timeseries_df = future_timeseries_df.set_index('yyyy-mm-dd')
+future_timeseries_df['yyyymmdd'] = pd.to_datetime(future_timeseries_df['yyyymmdd'])
+future_timeseries_df = future_timeseries_df.set_index('yyyymmdd')
 
 
 full_future_date_range = pd.date_range(start=future_timeseries_df.index.min(), end=future_timeseries_df.index.max(),freq='D')  # full date range including leap years
 future_df_reindexed = future_timeseries_df.reindex(full_future_date_range)      # reindex, leap year rows for 2/29 are filled with nan
 future_final_df = future_df_reindexed.interpolate(method='linear')              # nans filled with linear interpolation
 
-future_final_df.to_csv(output_filename, index=True, index_label="yyyy-mm-dd")
+future_final_df.to_csv(output_filename, index=True, index_label="yyyymmdd")
 
 
